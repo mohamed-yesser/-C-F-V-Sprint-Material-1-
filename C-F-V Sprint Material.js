39d38331d363485b937a5734842ac208829d7933
mohamed-yesser <159530889+mohamed-yesser@users.noreply.github.com>
2024-02-11 21:21:55 +0100

    Add files via upload

---
+++ C-F-V Sprint Material.js
@@ -0,0 +1,90 @@
+var carName = Volvo 
+
+//2
+
+var length = 16;
+// number
+
+var lastName = "Johnson"; 
+//string
+
+var isGreaterThan10 = length > 10 
+//boolean 
+
+
+//3
+
+function square1 (x) { 
+    return x * x  
+    } 
+
+function square2 (x) {
+    return x * x  
+    }
+
+function square3 (x) { 
+        return x * x
+    }
+
+//4 
+
+function largest(a , b) {
+  if ( a > b )
+    return a 
+  else 
+    return b 
+}
+
+
+//5
+
+function add(number1, number2) {
+    return number1 + number2
+
+}
+
+
+//6 
+
+function subtract (number1, number2) {
+    return number1 - number2
+
+}
+
+//7 
+
+var Operator = '+'
+
+var c = 10 
+var d = 6 
+
+function addOrSub (op) {
+if ( op === Operator ) 
+{
+    console.log(add(c , d ))
+     return add ( c , d) }
+    
+ else    
+    console.log(subtract(c , d))
+    return subtract ( c , d)
+    
+
+}
+
+
+//8 
+
+function divide(num1 , num2 ) {
+
+    return num1 / num2
+   
+}
+
+function multiply(num1 , num2 ) {
+
+    return num1 * num2    
+}
+
+
+
+
